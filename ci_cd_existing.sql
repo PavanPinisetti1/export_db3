df84f1488a0821123709727fd5d610eb6a55510c
Pavan Pinisetti <pavan.pinisetti@winfosolutions.com>
2025-08-19 12:14:01 +0530

    commit to 1.1

--- ci_cd_existing.sql
+++ ci_cd_existing.sql
@@ -1,11 +1,11 @@
-project export
+-- project export
 
 !git add .
-!git commit -m "Base release"
+!git commit -m "2nd Base release"
 
 !git branch -M main
 !git push -u origin main
-!git checkout -B base-release
+-- !git checkout -B base-release
 !git push -u origin base-release
 
 --exit
